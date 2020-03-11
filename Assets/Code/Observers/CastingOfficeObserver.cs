794f8e6f31020e2c6d51ea36e67f8a43d365ab6a
gabe <cofflag@wwu.edu>
2020-03-10 18:28:23 -0700

    board fix?

--- Assets/Code/Observers/CastingOfficeObserver.cs
+++ Assets/Code/Observers/CastingOfficeObserver.cs
@@ -2,20 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.UI;
 
 namespace DeadWood
 {
-    public class CastingOfficeObserver : Observer
+    public class CastingOfficeObserver : MonoBehaviour
     {
+
+        public Text textBox;
 
         // Update is called once per frame
         void Update()
         {
             HashSet<Tuple<int,int,int>> upgrades = Controller.cont.GetPlayerUpgrades();
-            textBox.text = "";
+            textBox.text = "Rank:" + "\t" + "Dollars:" + "\t" + "Credits:" + "\n";
             foreach(Tuple<int,int,int> t in upgrades)
             {
-                textBox.text = textBox.text + t.Item1 + "\t" + t.Item2 + "\t" + t.Item3 + "\n";
+                textBox.text = textBox.text + t.Item1 + "\t\t\t" + t.Item2 + "\t\t\t" + (t.Item1==2?"\t":"") + t.Item3 + "\n";
             }
         }
     }
