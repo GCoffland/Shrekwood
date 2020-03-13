b3a9072499c31de00646992d07d087a855b2efad
Jason Warren Cook <jasonwarrencook@gmail.com>
2020-03-13 09:29:24 -0700

    Saving these before corona shuts off my electricity

--- Assets/Code/Controller/ControllerBehavior.cs
+++ Assets/Code/Controller/ControllerBehavior.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class ControllerBehavior : MonoBehaviour
@@ -8,6 +9,28 @@ public class ControllerBehavior : MonoBehaviour
     public string currentPlayer;
     public string currentPlayerLocation;
 
+    [SerializeField]
+    private AudioClip ahh;
+
+    [SerializeField]
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private Text Move;
+
+    [SerializeField]
+    private Text Act;
+
+    [SerializeField]
+    private Text Rehearse;
+
+    [SerializeField]
+    private Text End;
+
+    private Text SelectedOption;
+
+    private Text[] optionsArray = new Text[4];
+    private int optionLocation = 0; //Start on move!
 
     // Start is called before the first frame update
     void Awake()
@@ -19,18 +42,91 @@ public class ControllerBehavior : MonoBehaviour
             GameObject temp = GameObject.Instantiate<GameObject>(Resources.Load<GameObject>("Prefabs/Player" + i));
             temp.GetComponent<PlayerObserver>().loadPlayer();
         }
+        optionsArray[0] = Move;
+        optionsArray[1] = Act;
+        optionsArray[2] = Rehearse;
+        optionsArray[3] = End;
+
+        SelectedOption = optionsArray[0];
+
     }
 
+    void Start()
+    {
+        SelectOption(0);
+    }
+
+    void SelectOption(int location)
+    {
+        // Reset the old option!
+        SelectedOption.color = Color.white;
+        SelectedOption.GetComponent<Outline>().effectColor = Color.black;
+        // Set the new option
+        SelectedOption = optionsArray[location];
+        SelectedOption.fontStyle = FontStyle.Bold;
+        SelectedOption.color = Color.black;
+        SelectedOption.GetComponent<Outline>().effectColor = Color.white;
+        audioSource.PlayOneShot(ahh, 1f);
+    }
+
+    void DoCurrentOption()
+    {
+        switch(optionLocation)
+        {
+            case 0:
+                Debug.Log("MOVE");
+                break;
+            case 1:
+                Debug.Log("ACT");
+                break;
+            case 2:
+                Debug.Log("REEEEE");
+                break;
+            case 3:
+                Debug.Log("END");
+                break;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
         cont.GameUpdate();
+        /*
+         * //This was for a test, instead this will control the menu options on the right
         if (Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f)
         {
             cont.Move(cont.gameState.currentPlayer, "Castle");
-            
-        }
+        }*/
         currentPlayer = cont.gameState.currentPlayer.playerName;
         currentPlayerLocation = cont.gameState.currentPlayer.currentLocation.name;
+
+        if (Input.GetButtonDown("Vertical") && Input.GetAxisRaw("Vertical") < 0) //Arrow down
+        {
+            if (optionLocation < 3)
+            {
+                optionLocation++;
+                SelectOption(optionLocation);
+            }
+            else
+            {
+                audioSource.PlayOneShot(ahh, 0.2f);
+            }
+        }
+        if (Input.GetButtonDown("Vertical") && Input.GetAxisRaw("Vertical") > 0) //Arrow up
+        {
+            if (optionLocation > 0)
+            {
+                optionLocation--;
+                SelectOption(optionLocation);
+            }
+            else
+            {
+                audioSource.PlayOneShot(ahh, 0.2f);
+            }
+        }
+        if (Input.GetButtonDown("Start"))
+        {
+            DoCurrentOption();
+        }
     }
 }
